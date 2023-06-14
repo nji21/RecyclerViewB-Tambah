722fd3f6138e0be113b0b362ab67c8852e369f93
nji21 <mateuspanji6@gmail.com>
2023-06-14 11:13:54 +0700

    MTPJ

---
+++ app/src/main/java/com/mateuspanji/recyclerviewb/AddMahasiswaActivity.java
@@ -0,0 +1,15 @@
+package com.mateuspanji.recyclerviewb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMahasiswaActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mahasiswa);
+    }
+}
